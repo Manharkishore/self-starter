1d7f090d5a4ca73b3875e6aef7e2728a05c4707e
manhark <manhark>
2018-08-22 18:03:24 +0530

    header

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import Header from './components/common/Header';
+import List from './components/common/list/List';
+import './index.css';
 const App = () =>{
-    const title = 'React Coin';
+
 
     return(
         <div>
             <Header />
-        <h1>{title}</h1>
+            <List />
         </div>
     );
 }
